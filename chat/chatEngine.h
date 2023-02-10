b287ed4d9b5f20c88998ec9efb5998686c6e1fd3
Rodger0208 <rodger02081990@gmail.com>
2023-02-10 17:39:18 +0300

    Merge branch 'marat-mak:master' into master

--- chat/chatEngine.h
+++ chat/chatEngine.h
@@ -29,28 +29,28 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 void reg()
 {
 	SetConsoleTextAttribute(hConsole, 11);
-	cout << "Введите имя пользователя: ";
+	cout << "Enter username: ";
 	cin >> name;
 	for (int i = 0; i < users.getSize(); i++)
 	{
-		if (users[i].getName() == name) throw "пользователь с таким именем уже существует";
+		if (users[i].getName() == name) throw "name is busy";
 	}
 	cout << "Enter login: ";
 	cin >> login;
 	for (int i = 0; i < users.getSize(); i++)
 	{
-		if (users[i].getLogin() == login) throw "данный логин занят";
+		if (users[i].getLogin() == login) throw "login is busy";
 	}
 	while (true)
 	{
-		cout << "EВведите пароль (минимум 6 символов): " << endl;
+		cout << "Enter password (min 6 characters): " << endl;
 		cin >> password;
 
 		if (password.length() <= 5)
 		{
 			SetConsoleTextAttribute(hConsole, 12);
-			cout << "пароль слишком короткий" << endl << endl;
-			cout << "нажмите q для возврата в меню" << endl << endl;
+			cout << "the password is too short" << endl << endl;
+			cout << "press q to exit" << endl << endl;
 
 			if (password == "q")
 			{
@@ -67,21 +67,21 @@ void reg()
 bool signUp()
 {
 	SetConsoleTextAttribute(hConsole, 13);
-	cout << "Введите логин: ";
+	cout << "Enter login: ";
 	cin >> login;
-	cout << "Введите пароль: ";
+	cout << "Enter password: ";
 	cin >> password;
 	for (int i = 0; i < users.getSize(); ++i)
 	{
 		if (users[i].getLogin() == login && users[i].getPassword() == password)
 		{
 			system("cls");
-			cout << "Привет " << users[i].getName() << "!" << endl;
+			cout << "Login correct! Hello " << users[i].getName() << endl;
 			currentUser = users[i].getName();
 			return true;
-		}					
+		}
 	}
-	throw "неверный логин или пароль";
+	throw "incorrect login or password";
 }
 
 
@@ -89,13 +89,13 @@ bool signUp()
 void userMenu()
 {
 	SetConsoleTextAttribute(hConsole, 14);
-	cout << "Введите [r] - прочитать сообщения; [w] - написать сообщение; [s] - вывести список пользователей; любой другой символ для возврата в главное меню" << endl;
+	cout << "Press [r] - read message; [w] - write message; [s] - show users; [any] - exit to main menu" << endl;
 	char ch = 'null';
 	cin >> ch;
 	switch (ch)
 	{
 	case 'r':
-		
+
 		readMessage();
 		break;
 	case 'w':
@@ -116,7 +116,7 @@ void userMenu()
 
 void readMessage()
 {
-	 
+
 	for (int i = 0; i < messages.getSize(); i++)
 	{
 		if (messages[i].getTo() == currentUser || messages[i].getTo() == "all")
@@ -127,14 +127,14 @@ void readMessage()
 
 void writeMessage()
 {
-	
+
 	string to;
 	string sms;
 	bool test = false;
 	SetConsoleTextAttribute(hConsole, 9);
-	cout << "введите имя адресата(или введите all чтобы отправить сообщение всем пользователям)\n: ";
+	cout << "to whom(write 'all' for all): ";
 	cin >> to;
-  cin.ignore();
+	cin.ignore();
 	for (int i = 0; i < users.getSize(); i++)
 	{
 		if (users[i].getName() == to || to == "all")
@@ -143,8 +143,8 @@ void writeMessage()
 			continue;
 		}
 	}
-	if (test == false) throw "пользователя с таким именем нет в базе чата, проверьте список пользователей";
-	cout << "введите сообщение: ";
+	if (test == false) throw "no user in base";
+	cout << "enter message: ";
 	getline(cin, sms);
 	messages.addItem(Message(currentUser, to, sms));
 	userMenu();
@@ -154,7 +154,7 @@ void showUsers()
 {
 	SetConsoleTextAttribute(hConsole, 11);
 	cout << "=============================================" << endl;
-	for (int i = 0; i < users.getSize();i++)
+	for (int i = 0; i < users.getSize(); i++)
 	{
 		cout << i + 1 << ". " << users[i].getName() << endl;
 	}
