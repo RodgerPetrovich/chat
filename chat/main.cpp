fe0f2bae66c53cf6738be9c25db7989d19abb617
Rodger0208 <rodger02081990@gmail.com>
2023-02-10 17:39:55 +0300

    11

--- chat/main.cpp
+++ chat/main.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	char choice = 'null';
 	while (choice != 'q')
 	{
 		SetConsoleTextAttribute(hConsole, 11);
-		cout << "Добро пожаловать в чат!" << endl;
-		cout << "Введите [s] - для входа; [r] - регистрации; [q] - выхода" << endl;
+		cout << "Welcome to chat" << endl;
+		cout << "Press [s] - sign up; [r] - registry new user; [q] - quit" << endl;
 		cin >> choice;
 		switch (choice)
 		{
@@ -25,8 +22,8 @@ int main()
 				{
 					userMenu();
 				}
-			}			
-			catch (const char* ex)            
+			}
+			catch (const char* ex)
 			{
 				cout << ex << endl;
 			}
@@ -43,6 +40,6 @@ int main()
 		}
 		cout << "============================================\n";
 	}
-	
+
 	return 0;
 }
